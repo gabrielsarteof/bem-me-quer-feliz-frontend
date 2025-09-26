8d69a2f0149d3f6d7ff80ec9f9c6b00e68052801
ghabriel <souzaghabriel195@gmail.com>
2025-09-25 22:20:03 -0300

    refactor(ui):tirando componente do page.tsx e removendo pasta de fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import EventsSection from '@/components/sections/EventsSection';
-import StorySection from '@/components/sections/StorySection';
-
 export default function Home() {
   return (
     <div>
-      <main>
-        <StorySection />
-      </main>
+      <main></main>
     </div>
   );
 }
